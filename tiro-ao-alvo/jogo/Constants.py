a4a6ccb2240b156c4498bb9549cf1b28eb6d68a5
CamilaSilvaC <164106587+CamilaSilvaC@users.noreply.github.com>
2025-01-08 19:22:43 -0300

    Add files via upload

--- tiro-ao-alvo/jogo/Constants.py
+++ tiro-ao-alvo/jogo/Constants.py
@@ -1,11 +1,17 @@
+
+import os
+
+# Diretório atual do arquivo Constants.py
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Caminhos relativos à pasta 'assets'
+BG = os.path.join(BASE_DIR, '../assets/images/bg.webp')
+FONTE = os.path.join(BASE_DIR, '../assets/fonts/PixelGameFont.ttf')
+ALVO = os.path.join(BASE_DIR, '../assets/images/target.png')
+MIRA = os.path.join(BASE_DIR, '../assets/images/mouse.png')
+DISPARO = os.path.join(BASE_DIR, '../assets/audio/disparo.mp3')
+
+
 #tamanho da tela
 LARGURA = 1080
 ALTURA = 620
-
-#arquivos
-BG = 'assets/images/bg.webp'
-FONTE = 'assets/fonts/PixelGameFont.ttf'
-ALVO = 'assets/images/target.png'
-MIRA = 'assets/images/mouse.png'
-DISPARO = 'assets/audio/disparo.mp3'
-
